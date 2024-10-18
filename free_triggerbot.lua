d1397ec8db1531a9cacf709ade3f9bdd919a7ede
jamkless <jamkles@innocent.com>
2024-10-18 07:16:44 +0100

    Update free_triggerbot.lua

--- free_triggerbot.lua
+++ free_triggerbot.lua
@@ -1,1 +1,18 @@
-
+getgenv().Script = {
+    TriggerBot = {
+        Enabled = true,
+        ClickDelay = 0,
+    },
+    
+    FOV = {
+            Circle = {
+            Visible = true,
+            Color = Color3.fromRGB(255, 255, 255),
+            Transparency = 1,
+            Thickness = 1.5,
+            NumSides = 1000000,
+            Radius = 900,
+            Filled = false,
+        },
+    }
+}
